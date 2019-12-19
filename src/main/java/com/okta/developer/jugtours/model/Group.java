b5d9024942839435ed9ebe0d863e7a2858fbbe5b
Kien Nguyen <kienkun1990@gmail.com>
2019-12-19 09:20:56 +0700

    spring and react

--- src/main/java/com/okta/developer/jugtours/model/Group.java
+++ src/main/java/com/okta/developer/jugtours/model/Group.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class Group {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NonNull
     private String name;
